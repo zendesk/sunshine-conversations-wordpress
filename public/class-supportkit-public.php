b25cea0ae3c81eedbe362d4941d0037e7a29f703
mike gozzo <mgozzo@gmail.com>
2015-10-01 14:11:37 -0400

    settings

--- public/class-supportkit-public.php
+++ public/class-supportkit-public.php
@@ -118,7 +118,8 @@ class Supportkit_Public {
 		 * class.
 		 */
 		$options = get_option( $this->plugin_name . '-options' );
-
+		echo "<h1> SUPPORTKIT! </h1>";
+		echo $options[app-token]
 		?>
 		<!-- SK Init -->
 		<script>SupportKit.init({appToken: '<?php echo($options[app-token]);?>'});</script>";
